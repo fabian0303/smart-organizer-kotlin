5bdddb7db25c4ab23aec00d6b1a0e2890051e68e
Javier Aliaga <reivaj_31@hotmail.com>
2020-07-03 14:07:24 -0300

    Agrego listview de notas al fragment con su adaptador.

--- OrganizadorUniversitario/app/src/main/java/tecnologiasmoviles/organizadoruniversitario/NotasAdapter.kt
+++ OrganizadorUniversitario/app/src/main/java/tecnologiasmoviles/organizadoruniversitario/NotasAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.item_nota.view.*
+import tecnologiasmoviles.organizadoruniversitario.my_fragment.FragmentNota
 
-class NotasAdapter(private val mContext: Context, private val listaNotas: List<Nota>):ArrayAdapter<Nota>(mContext,0,listaNotas){
+    class NotasAdapter(private val mContext: FragmentActivity, private val listaNotas: List<Nota>):ArrayAdapter<Nota>(mContext,0,listaNotas){
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val layout = LayoutInflater.from(mContext).inflate(R.layout.item_nota,parent,false)
