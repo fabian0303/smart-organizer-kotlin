ef6a2c5d7287b21c4fbe10d7d187908ffb54ab67
Fabian Oyarce <faoyarce15@alumnos.utalca.cl>
2020-07-14 14:44:14 -0400

    Base de datos ROM
    Falta refinar detalles

---
+++ OrganizadorUniversitario/app/src/main/java/tecnologiasmoviles/organizadoruniversitario/AppDatabase.kt
@@ -0,0 +1,30 @@
+package tecnologiasmoviles.organizadoruniversitario
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+
+@Database(entities = [Curso::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun cursoDao(): CursoDao
+
+    companion object{
+
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if (INSTANCE == null){
+               INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "smart-organizer-db"
+                ).build()
+            }
+            return  INSTANCE!!
+        }
+    }
+
+}
