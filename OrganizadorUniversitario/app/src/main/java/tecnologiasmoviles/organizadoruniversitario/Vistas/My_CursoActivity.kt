a38ac210f1a6bb03891cbdfc2f094c90daf1f633
brayan <brayan.escobar@live.com>
2020-07-28 23:18:50 -0400

    Home de cursos.

---
+++ OrganizadorUniversitario/app/src/main/java/tecnologiasmoviles/organizadoruniversitario/Vistas/My_CursoActivity.kt
@@ -0,0 +1,11 @@
+package tecnologiasmoviles.organizadoruniversitario.Vistas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class My_CursoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my__curso)
+    }
+}
