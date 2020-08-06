2391b795ac2fa3e878e496a80881222ef4a5506a
brayan <brayan.escobar@live.com>
2020-08-05 22:51:18 -0400

    Tomar ftos v1

--- OrganizadorUniversitario/app/src/main/java/tecnologiasmoviles/organizadoruniversitario/Vistas/NavegacionCurso/OpcionesImagenActivity.kt
+++ OrganizadorUniversitario/app/src/main/java/tecnologiasmoviles/organizadoruniversitario/Vistas/NavegacionCurso/OpcionesImagenActivity.kt
@@ -2,10 +2,13 @@ package tecnologiasmoviles.organizadoruniversitario.Vistas.NavegacionCurso
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.ActionMenuView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +17,8 @@ import tecnologiasmoviles.organizadoruniversitario.R
 
 class OpcionesImagenActivity : AppCompatActivity() {
     private val REQUEST_GALERIA = 1001
+    private val REQUEST_FOTO = 1002
+    var foto: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opciones_imagen)
@@ -34,6 +39,7 @@ class OpcionesImagenActivity : AppCompatActivity() {
             }
         }
 
+        tomarFotografia()
     }
 //verificamos si el usuario le dio permiso a la app, para acceder a la galeria.
     override fun onRequestPermissionsResult( requestCode: Int, permissions: Array<out String>, grantResults: IntArray ) {
@@ -45,8 +51,16 @@ class OpcionesImagenActivity : AppCompatActivity() {
                 }else{
                     Toast.makeText(applicationContext,"No hay permisos para acceder Galeria de imagenes", Toast.LENGTH_LONG).show()
                 }
-
             }
+            REQUEST_FOTO -> {
+                if(grantResults[0] ==PackageManager.PERMISSION_GRANTED ){
+                    abrirCamara()
+                }else{
+                    Toast.makeText(applicationContext,"No hay permisos para abrir la camara", Toast.LENGTH_LONG).show()
+
+                }
+            }
+
         }
     }
     private fun mostrarGaleria(){
@@ -61,5 +75,34 @@ class OpcionesImagenActivity : AppCompatActivity() {
         if(resultCode == Activity.RESULT_OK && requestCode  == REQUEST_GALERIA){
             viewImagen.setImageURI(data?.data)
         }
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_FOTO){
+            viewImagen.setImageURI(foto)
+        }
+    }
+
+    private fun tomarFotografia(){
+      tomarFoto.setOnClickListener {
+          if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+              if ( checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+                  || checkSelfPermission( Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                  //pedimos permiso
+                  val permisosCamara = arrayOf( Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                  requestPermissions(permisosCamara,REQUEST_FOTO)
+              }else{
+                  abrirCamara()
+              }
+          }else{
+              abrirCamara()
+          }
+      }
+    }
+    // abrir la camara del telefono
+    private  fun abrirCamara(){
+        val value = ContentValues()
+        value.put(MediaStore.Images.Media.TITLE, "Nueva imagen")
+        foto = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, value)
+        val camaraIntent  = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        camaraIntent.putExtra(MediaStore.EXTRA_OUTPUT, foto)
+        startActivityForResult(camaraIntent,REQUEST_FOTO)
     }
 }
